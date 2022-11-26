ac7a0e5aee1b60b6a1f89dbca044476998ba19be
Moon Yong Joon <myjlms99@gmail.com>
2022-11-26 14:50:02 +0900

    88889999111

--- hello_num/src/main.rs
+++ hello_num/src/main.rs
@@ -5,5 +5,15 @@ mod user;
 fn main() {
     println!("Hello, world!");
 
-    println!(" { } ", add(100,200))
+    println!(" { } ", add(100,200));
+
+    let x = 5;
+    println!("The value of x is: {}", x);
+    let x = 6;
+    println!("The value of x is: {}", x);
+
+    let mut y = 100;
+    println!("mutable value {} ", y);
+    y = 999;
+    println!("mutable value {} ", y);
 }
